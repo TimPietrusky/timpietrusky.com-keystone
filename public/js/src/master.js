37c64a52405fe7793640e9c105b32d5b586a4e21
TimPietrusky <timpietrusky@gmail.com>
2015-11-30 23:35:46 +0100

    Updated 2015.

--- public/js/src/master.js
+++ public/js/src/master.js
@@ -20,3 +20,22 @@ var myLoader = new LazyLuke({
   elements : '[data-js-src]',
   srcAttribut : 'data-js-src'
 });
+
+
+
+// var content_elements = document.querySelectorAll('[data-js="element"]');
+
+// // There are content elements on the current page
+// if (content_elements.length > 0) {
+
+//   for (var i = 0; i < content_elements.length; i++) {
+//     content_elements[i].addEventListener(
+//       'click',
+//       function(e) {
+//         e.preventDefault();
+//       },
+//       false
+//     );
+
+//   };
+// }
